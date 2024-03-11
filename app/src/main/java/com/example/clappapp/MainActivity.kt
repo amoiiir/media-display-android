219195546cdaef0ba3dc0c8ad24b0410fcbf81b3
Amir Hamzah <mahamzah535@gmail.com>
2024-03-11 10:05:09 +0800

    Added time remaining

--- app/src/main/java/com/example/clappapp/MainActivity.kt
+++ app/src/main/java/com/example/clappapp/MainActivity.kt
@@ -2,8 +2,11 @@ package com.example.clappapp
 
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,6 +16,8 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 class MainActivity : AppCompatActivity() {
     private lateinit var seekBar: SeekBar
     private var mediaPlayer: MediaPlayer? = null
+    private lateinit var runnable: Runnable //instances that is needed to be implemented
+    private lateinit var handler: Handler
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
@@ -20,6 +25,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         seekBar = findViewById(R.id.sbBark)
+        handler = Handler(Looper.getMainLooper())
 
         val play = findViewById<FloatingActionButton>(R.id.fabPlay)
         val pause = findViewById<FloatingActionButton>(R.id.fabPause)
@@ -29,6 +35,7 @@ class MainActivity : AppCompatActivity() {
             if(mediaPlayer==null){
                 //initialize the media player in the onCreate()
                 mediaPlayer = MediaPlayer.create(this, R.raw.dog)
+                initializeSeekBar()
             }
 
             mediaPlayer?.start()
@@ -43,6 +50,8 @@ class MainActivity : AppCompatActivity() {
             mediaPlayer?.reset()
             mediaPlayer?.release()
             mediaPlayer = null
+            handler.removeCallbacks(runnable)
+            seekBar.progress = 0
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -53,6 +62,36 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initializeSeekBar(){
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) mediaPlayer?.seekTo(progress)
+            }
 
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+        })
+        val tvPlayed = findViewById<TextView>(R.id.tvPlayed)
+        val tvDue = findViewById<TextView>(R.id.tvDue)
+        seekBar.max = mediaPlayer!!.duration //null checking using not null
+        //initialize using runnable
+        runnable = Runnable {
+            seekBar.progress = mediaPlayer!!.currentPosition
+
+            val playedTime = mediaPlayer!!.currentPosition/1000
+            tvPlayed.text = "$playedTime sec"
+
+            val duration = mediaPlayer!!.duration/1000
+            val dueTime = duration - playedTime
+            tvDue.text = "$dueTime sec"
+
+            handler.postDelayed(runnable,1000)
+        }
+        handler.postDelayed(runnable,1000)
     }
 }
